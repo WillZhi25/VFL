292a79d1dab62e2f149a30ced9883000f756217e
Will <wzhi@xogrp.com>
2015-04-15 11:00:51 +0800

    first commit

---
+++ VFL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VFL
+//
+//  Created by Will Zhi on 15/4/15.
+//  Copyright (c) 2015年 机智的新手. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VFL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VFL
+//
+//  Created by Will Zhi on 15/4/15.
+//  Copyright (c) 2015年 机智的新手. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
